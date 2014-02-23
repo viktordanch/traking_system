d3ce8312ebc87beecf619429732ece1b363d8ed3
viktordanch <viktor.danch@gmail.com>
2014-02-23 13:50:59 +0200

    create app, add devise

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TrakingApp::Application.config.secret_key_base = '598beff44ae31370f9475d063064d068f38c3b9a2b02e9b6a4555e209bbf4bf7a16ea42d20c8ef3db971a0fbd8b644f64a9035d6413d71910c7c4d2857c66c23'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TrakingApp::Application.config.session_store :cookie_store, key: '_traking_app_session'
